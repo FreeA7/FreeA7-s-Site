c8814ca4d939909f8cbbbb02d06a717ba5e1c726
FreeA7 <987320967@qq.com>
2021-11-04 23:12:18 +0800

    增加孙可相关文章;增加备案号链接到工信部网站;增加看板娘OC

--- motify_index.py
+++ motify_index.py
@@ -11,6 +11,11 @@ Created on Mon Nov  1 21:04:31 2021
 import os
 
 htmls = set()
+live2d_js = '''<script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css"/>
+      <script src="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js"></script>
+      '''
+
 
 for root, dirs, files in os.walk('./site'):
     for f in files:
@@ -21,8 +26,9 @@ for filename in htmls:
     with open(filename, 'r', encoding = 'utf-8') as f:
         content = f.read()
         start = content.find('Material for MkDocs') + 32
-        content = content[:start] + '京ICP备20011564号-1' + content[start:]
+        content = content[:start] + '<a href="https://beian.miit.gov.cn/">&nbsp京ICP备20011564号-1</a>' + content[start:]
+        start = content.find('<script')
+        content = content[:start] + live2d_js + content[start:]
     with open(filename, 'w', encoding = 'utf-8') as f:
         f.write(content)
-        
         
